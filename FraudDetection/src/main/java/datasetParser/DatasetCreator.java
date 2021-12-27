aed41c3242797711a82de946f4739c42f7c3a047
Manos Chatzakis <chatzakis@ics.forth.gr>
2021-12-27 19:07:21 +0200

    added new project and dataset

---
+++ FraudDetection/src/main/java/datasetParser/DatasetCreator.java
@@ -0,0 +1,214 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datasetParser;
+
+import csv.CSV;
+import csv.CSVFactory;
+import csv.CSVLine;
+import csv.CSVPrinter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import utils.CommonUtils;
+
+/**
+ *
+ * @author Manos Chatzakis
+ */
+public class DatasetCreator {
+
+    static int ADRESS_INDEX = 0;
+    static int ENTITY_INDEX = 1;
+    static int OFFICER_INDEX = 2;
+    static int INTERMEDIARY_INDEX = 3;
+
+    static int[] CVSs2use = {0, 1, 2, 3};
+
+    static String startingCSVEdges = "data/csv_panama_papers.2018-02-14/panama_papers.edges.csv";
+
+    static String[] startingCSVs = {
+        "data/csv_panama_papers.2018-02-14/panama_papers.nodes.address.csv",
+        "data/csv_panama_papers.2018-02-14/panama_papers.nodes.entity.csv",
+        "data/csv_panama_papers.2018-02-14/panama_papers.nodes.officer.csv",
+        "data/csv_panama_papers.2018-02-14/panama_papers.nodes.intermediary.csv"
+    };
+
+    static String[] typeNames = {
+        "address",
+        "entity",
+        "officer",
+        "intermediary"
+    };
+
+    static int[][] startingCSVsInterestedCols = {
+        {0, 2, 3},
+        {0, 1, 4},
+        {0, 1, 2},
+        {0, 1, 2}
+    };
+
+    String[] outputCSVs = {
+        "data/csv_panama_papers.2018-02-14/panama_papers.nodes.address_simplified.csv",
+        "data/csv_panama_papers.2018-02-14/panama_papers.nodes.entity_simplified.csv",
+        "data/csv_panama_papers.2018-02-14/panama_papers.nodes.officer_simplified.csv",
+        "data/csv_panama_papers.2018-02-14/panama_papers.nodes.intermediary_simplified.csv"
+    };
+
+    public static void createDatasetSampling() throws FileNotFoundException, IOException, CloneNotSupportedException {
+        /*Step 1: Get some edges from the file and save the IDs in a set*/
+        CSV edges = new CSV(startingCSVEdges, ",", true);
+        edges.parse();
+
+        int min = 0;
+        int max = edges.getParsedLines().size();
+        int[] randcols = CommonUtils.generateRandomNums(4000, min, max);
+        //CSV selectedEdges = CSVFactory.selectCSVRowFactory(edges, 0, 4000);
+        CSV selectedEdges = CSVFactory.selectCSVRowFactory(edges, randcols);
+
+        ArrayList<CSVLine> lines = selectedEdges.getParsedLines();
+        Set<Integer> nodeIDs = new HashSet<>();
+        int index = 0;
+        String edgesOutput = "ID, " + selectedEdges.getHeader().toString();
+        for (CSVLine line : lines) {
+            int fromID = Integer.parseInt(line.getColumns(0, 0).get(0));
+            int toID = Integer.parseInt(line.getColumns(2, 2).get(0));
+
+            nodeIDs.add(toID);
+            nodeIDs.add(fromID);
+
+            index++;
+
+            edgesOutput += index + "," + line.toString();
+        }
+
+        CommonUtils.writeStringToFile(edgesOutput, startingCSVEdges.replace(".csv", "__selected.csv"));
+        System.out.println("================ \n Phase 1 Completed.\n================");
+
+        /*Step 2: Use the set to find the nodes from the file*/
+        edgesOutput = "Type, ID, Name/Address, CountryCode\n";
+        for (int i : CVSs2use) {
+            String currType = typeNames[i];
+            CSV currCSV = new CSV(startingCSVs[i], ",", true);
+            currCSV.parse();
+            int[] cols = startingCSVsInterestedCols[i];
+
+            currCSV.selectColumns(cols);
+            ArrayList<CSVLine> csvLines = currCSV.getParsedLines();
+
+            for (CSVLine csvLine : csvLines) {
+                int lineID = Integer.parseInt(csvLine.getColumns(0, 0).get(0).replace("\"", ""));
+
+                if (nodeIDs.contains(lineID)) {
+                    edgesOutput += currType + "," + csvLine.toString();
+                }
+
+            }
+        }
+
+        CommonUtils.writeStringToFile(edgesOutput, "data/csv_panama_papers.2018-02-14/nodes.csv");
+        System.out.println("================ \n Phase 2 Completed.\n================");
+
+    }
+
+    public static void createDatasetPaths() throws FileNotFoundException, IOException, CloneNotSupportedException {
+
+        int depth = 3;
+        CSV edges = new CSV(startingCSVEdges, ",", true);
+        edges.parse();
+        String output = edges.getHeader().toString();
+
+        //int startingNode = 11000007;
+        // int startingNode = 10071302;
+        int count = 0;
+        for (int startingNode : edges.getColumnAsInt(0).subList(400000, 500000)) {
+            System.out.println("========================");
+            count++;
+            if (count == 10000) {
+                break;
+            }
+            Set<Integer> nodeIDs = new HashSet<>();
+
+            ArrayList<ArrayList<Integer>> levelNodes = new ArrayList<>();
+            ArrayList<Integer> firstLevelNode = new ArrayList<>();
+            ArrayList<Integer> currentLevelNodes = null;
+
+            firstLevelNode.add(startingNode);
+            levelNodes.add(firstLevelNode);
+
+            int levelBFS = 0;
+            while (levelBFS < depth && !(currentLevelNodes = levelNodes.get(levelBFS)).isEmpty()) {
+                ArrayList<Integer> nextLevelNodes = new ArrayList<>();
+                for (Integer currentNode : currentLevelNodes) {
+                    String toPrint = "Current Node: " + currentNode + ": [";
+                    nodeIDs.add(currentNode);
+                    CSV groupNeighboursCSV = CSVFactory.selectCSVRowsByValueFactory(edges, 0, currentNode + "");
+                    ArrayList<CSVLine> lines = groupNeighboursCSV.getParsedLines();
+                    for (CSVLine line : lines) {
+                        int toID = Integer.parseInt(line.getColumns(2, 2).get(0));
+                        toPrint += toID + " ";
+                        if (!nodeIDs.contains(toID)) {
+                            nextLevelNodes.add(toID);
+                        }
+                        //should write to file
+
+                        output += line.toString();
+                    }
+                    System.out.println(toPrint + "]");
+
+                }
+
+                levelNodes.add(nextLevelNodes);
+                levelBFS++;
+            }
+        }
+
+        CommonUtils.writeStringToFile(output, startingCSVEdges.replace(".csv", "__selectedPATHS.csv"));
+    }
+
+    public static void mergeNodes() throws FileNotFoundException, IOException, CloneNotSupportedException {
+        String mergeOutput = "Type, ID, Name/Address, CountryCode\n";
+        for (int i : CVSs2use) {
+            String currType = typeNames[i];
+            CSV currCSV = new CSV(startingCSVs[i], ",", true);
+            currCSV.parse();
+            int[] cols = startingCSVsInterestedCols[i];
+
+            currCSV.selectColumns(cols);
+            ArrayList<CSVLine> csvLines = currCSV.getParsedLines();
+
+            for (CSVLine csvLine : csvLines) {
+                mergeOutput += currType + "," + csvLine.toString();
+            }
+
+            CommonUtils.writeStringToFile(mergeOutput, "data/csv_panama_papers.2018-02-14/nodesMerged.csv");
+        }
+    }
+
+    public static void shuffleCSV() throws FileNotFoundException, IOException {
+        CSV edges = new CSV(startingCSVEdges, ",", true);
+        edges.parse();
+
+        ArrayList<CSVLine> lines = edges.getParsedLines();
+        Collections.shuffle(lines);
+
+        edges.setParsedLines(lines);
+        edges.toFile("data/csv_panama_papers.2018-02-14/shuffledEdges.csv");
+    }
+
+    public static void main(String[] args) throws IOException, FileNotFoundException, CloneNotSupportedException {
+        createDatasetSampling();
+        //createDatasetPaths();
+        //shuffleCSV();
+        //mergeNodes();
+        //createDatasetPaths();
+    }
+
+}
---
+++ FraudDetection/src/main/java/utils/CommonUtils.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author manos
+ */
+public class CommonUtils {
+     public static int[] generateRandomNums(int count, int min, int max) {
+        ArrayList<Integer> randomNums = new ArrayList<>();
+        int randomNumsCount = 0;
+
+        while (randomNumsCount < count) {
+            int rand = generateRandomNum(min, max);
+            if (!randomNums.contains(rand)) {
+                randomNums.add(rand);
+                randomNumsCount++;
+            }
+        }
+        int[] res = randomNums.stream().mapToInt(i -> i).toArray();
+        return res;
+    }
+
+    public static Integer[] ArrayList2Array(ArrayList<Integer> list) {
+        return (Integer[]) list.toArray();
+    }
+
+    public static int generateRandomNum(int min, int max) {
+        //System.out.println("Random value in int from " + min + " to " + max + ":");
+        return (int) Math.floor(Math.random() * (max - min + 1) + min);
+        //System.out.println(random_int);
+    }
+
+    public static String writeStringToFile(String data, String filepath) {
+        File file = new File(filepath);
+
+        try (FileOutputStream fos = new FileOutputStream(file);
+                BufferedOutputStream bos = new BufferedOutputStream(fos)) {
+            byte[] bytes = data.getBytes();
+            bos.write(bytes);
+            bos.close();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return file.getAbsolutePath();
+    }
+}
